9bd162cae7e63333dd327c48c114f65ed73d1084
LukeRekt <lucasrsl12@gmail.com>
2022-02-04 23:17:21 -0300

    primeiro teste com fetch api

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+const filme = document.getElementById("teste");
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
@@ -14,3 +15,35 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+fetch(`../db.json`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }).then(resp => resp.json())
+    .then((data) => {
+        criarFilme(data);
+        //setServices(data.services);
+
+    })
+
+    function criarFilme(dados){
+        for(let i = 0; i < dados.length; i++){
+            console.log(dados[i]['descricao'])
+            filme.innerHTML += `<div class="item">
+            <div class="over-filme">
+                <div class="sobre-filme">
+                    <h5>${dados[i]['filme']}</h5>
+                    <p>Umbrella Academy acompanha os membros de uma família disfuncional de super-heróis enquanto eles trabalham juntos para resolver a misteriosa morte de seu pai e lidam com suas personalidades e habilidades divergentes. </p>
+                    <button class="botao" style="margin-top: 5px; padding: 8px; font-size: 20px;">Assistir Agora</button>
+                    <button class="botao" style="margin-top: 5px; padding: 8px; font-size: 20px;">Sobre</button>
+                </div>
+                
+            </div>
+            <img class="box-filme" src="img/mini1.jpg" alt="">
+        </div>`
+        }
+        
+       
+    }
